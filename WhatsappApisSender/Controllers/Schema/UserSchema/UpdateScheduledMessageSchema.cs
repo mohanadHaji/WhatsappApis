941b301e29a4c49bec05dc6e62ab06b72ae742d7
Mahdi <mehdi.rezaiguia6@gmail.com>
2025-04-08 07:19:46 +0100

    Add UpdateScheduledMessageSchema class to define the structure for updating scheduled messages.

---
+++ WhatsappApisSender/Controllers/Schema/UserSchema/UpdateScheduledMessageSchema.cs
@@ -0,0 +1,8 @@
+namespace WhatsappApisSender.Controllers.Schema.UserSchema
+{
+    public class UpdateScheduledMessageSchema
+    {
+        public string Id { get; set; }
+        public DateTime DueDateUTC { get; set; }
+    }
+} 
